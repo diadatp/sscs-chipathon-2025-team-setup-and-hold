56b731158f749929c23958f39d1106ed2dcfecb2
recursivetree <recursivetreemail@gmail.com>
2025-08-14 11:08:26 +0200

    fix: generate spice as .subcircuit

--- scripts/gds_to_spice.tcl
+++ scripts/gds_to_spice.tcl
@@ -8,13 +8,13 @@ select top cell
 extract do local
 extract all
 ext2spice format ngspice
+ext2spice lvs
 ext2spice cthresh infinite
 ext2spice -o "globalfoundries-pdk-libs-gf180mcu_sah_sc/cells/${cell_name}/gf180mcu_sah_sc_${cell_name}_1.spice"
 ext2sim labels on
 ext2sim
 extresist tolerance 10
 extresist
-ext2spice lvs
 ext2spice cthresh 0
 ext2spice extresist on
 ext2spice -o "globalfoundries-pdk-libs-gf180mcu_sah_sc/cells/${cell_name}/gf180mcu_sah_sc_${cell_name}_1.rc.spice"
